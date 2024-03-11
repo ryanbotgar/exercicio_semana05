ae760973caaf2e695f53eba1f0666151bd4b09c4
ryanbotgar <159447197+ryanbotgar@users.noreply.github.com>
2024-03-11 09:31:21 -0300

    Add files via upload

---
+++ semana 05/exercicio_07.js
@@ -0,0 +1,12 @@
+class Animal {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    descrever() {
+        console.log(`Este animal se chama ${this.name} e tem ${this.age} anos.`);
+    }
+}
+
+
